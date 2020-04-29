60f6237389ee2a368e07253e39be6aadb199f0bd
Nikita <madeev@itglobal.ru>
2020-04-29 21:43:05 +0300

    Поддержка IE11

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ var del = require("del");
 var uglify = require("gulp-uglify");
 var concat = require("gulp-concat");
 var htmlmin = require("gulp-htmlmin");
+var objectFitImages = require("postcss-object-fit-images");
 
 gulp.task("images", function () {
   return gulp
@@ -57,7 +58,7 @@ gulp.task("css", function () {
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
-    .pipe(postcss([autoprefixer()]))
+    .pipe(postcss([objectFitImages, autoprefixer()]))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
